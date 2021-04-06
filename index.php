2ea8509d7d869f0011045b64663477707a2cdbc1
Nafis Bin Reza <nafisbinreza@gmail.com>
2021-04-06 12:02:17 +0600

    Completed Banner Part, Modified previous works and Started Promo Part

--- index.php
+++ index.php
@@ -7,68 +7,101 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/style.css">
-    
+
     <title>Ride</title>
 </head>
 
 <body>
     <!-- Common Header -->
     <div id="header-navbar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding: 30px 0">
-        <div class="container">
-            <a class="navbar-brand" href="#"><img src="images/logo.png" alt=""></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Rver</a>
-                    </li>
-                    
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Company
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    
-                </ul>
-                <div class="d-flex">
-                    <button class="btn btn-outline-success" style="margin-right:10px" type="submit">Sign Up</button>
-                    <button class="btn btn-outline-success" type="submit">Log In</button>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding: 10px 0">
+            <div class="container">
+                <a class="navbar-brand" href="#"><img src="images/logo.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Rver</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Company
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">About</a></li>
+                                <li><a class="dropdown-item" href="#">Riding</a></li>
+                                <li><a class="dropdown-item" href="#">Career</a></li>
+                                <li><a class="dropdown-item" href="#">Feedback</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Chat</a></li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                    <div class="d-flex">
+                        <button class="btn btn-outline-success" style="margin-right:10px" type="submit">Sign Up</button>
+                        <button class="btn btn-outline-success" type="submit">Log In</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
+        </nav>
     </div>
 
 
     <!-- Banner Part Start -->
-    <div id="banner">
-        <div class="banner_image">
-            <h1>asdasd</h1>
-            <h1>asdasd</h1>
-            <h1>asdasd</h1>
-        </div>    
-    
+    <div id="banner" class="text-white">
+        <div class="banner_overlay">
+            <div class="container">
+                <div class="banner_content">
+                    <h1>Book Rickshaws with Rver</h1>
+                    <p>Rver is the first rickshaw hiring service of Bangladesh. We help you to have a nice and safe
+                        journey.
+                    </p>
+                    <ul class="d-flex justify-content-start">
+                        <li><a href="#">Ride</a></li>
+                        <li><a href="#">Earn</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 
-
+    <!-- Banner Part Ends-->
+    <!-- Promo Part Starts-->
+    <div id="promo">
+        <div class="container">
+            <div class="col-lg-6">
+                <div class="promo_content">
+                    <img src="images/promo_pic1.jpg" alt="">
+                    <h3>Convenient</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolores autem reiciendis, nisi
+                        quod voluptatem. Expedita tenetur quas tempore? Minus.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
 
     <!-- Custom JS -->
     <script src="js/custom.js"></script>
